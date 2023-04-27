4f274dc591547513a360cd65552268f7d815fdb4
Paal Joergensen <pal.jorgensen@gmail.com>
2023-04-27 11:29:55 +0200

    Update node modules

--- webpack.config.js
+++ webpack.config.js
@@ -1,75 +1,75 @@
-const path = require('path');
-const webpack = require('webpack');
-const nodeEnv = process.env.NODE_ENV || 'development';
-const { VueLoaderPlugin } = require('vue-loader');
-const libraryName = process.env.npm_package_name;
+module.exports = (env, argv) => {
+  const path = require('path');
+  const webpack = require('webpack');
+  const mode = argv.mode;
+  const { VueLoaderPlugin } = require('vue-loader');
+  const libraryName = process.env.npm_package_name;
 
-const config = {
-  mode: nodeEnv,
-  context: path.resolve(__dirname, 'src'),
-  entry: "./entries/dist.js",
-  devtool: (nodeEnv === 'production') ? undefined : 'inline-source-map',
-  output: {
-    path: path.join(__dirname, 'dist'),
-    filename: `${libraryName}.js`,
-    sourceMapFilename: '[file].map'
-  },
-  resolve: {
-    extensions: ['.tsx', '.ts', '.js', '.vue'],
-    alias: {
-      'vue': '@vue/runtime-dom'
+  return config = {
+    mode: mode,
+    context: path.resolve(__dirname, 'src'),
+    entry: "./entries/dist.js",
+    devtool: (mode === 'production') ? undefined : 'inline-source-map',
+    output: {
+      path: path.join(__dirname, 'dist'),
+      filename: `${libraryName}.js`,
+      sourceMapFilename: '[file].map'
     },
-    modules: [
-      path.resolve('./src'),
-      path.resolve('./node_modules')
-    ],
-    fallback: {
-      util: require.resolve("util/")
-    }
-  },
-  module: {
-    rules: [
-      {
-        test: /\.vue$/,
-        loader: 'vue-loader',
-        options: {
-          preserveWhitespace: false,
+    resolve: {
+      extensions: ['.tsx', '.ts', '.js', '.vue'],
+      alias: {
+        'vue': '@vue/runtime-dom'
+      },
+      modules: [
+        path.resolve('./src'),
+        path.resolve('./node_modules')
+      ],
+      fallback: {
+        util: require.resolve("util/")
+      }
+    },
+    module: {
+      rules: [
+        {
+          test: /\.vue$/,
+          loader: 'vue-loader',
+          options: {
+            preserveWhitespace: false,
+          },
         },
-      },
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: 'babel-loader'
-      },
-      {
-        test: /\.(s[ac]ss|css)$/,
-        use: [
-          'vue-style-loader',
-          'css-loader',
-          'sass-loader'
-        ]
-      },
-      {
-        test: /\.(svg)$/,
-        include: path.join(__dirname, 'src/images'),
-        use: [
-          {
-            loader: 'url-loader',
-            options: {
-              limit: 10000
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: 'babel-loader'
+        },
+        {
+          test: /\.(s[ac]ss|css)$/,
+          use: [
+            'vue-style-loader',
+            'css-loader',
+            'sass-loader'
+          ]
+        },
+        {
+          test: /\.(svg)$/,
+          include: path.join(__dirname, 'src/images'),
+          use: [
+            {
+              loader: 'url-loader',
+              options: {
+                limit: 10000
+              }
             }
-          }
-        ]
-      } // inline base64 URLs for <=10k images, direct URLs for the rest
+          ]
+        } // inline base64 URLs for <=10k images, direct URLs for the rest
+      ]
+    },
+    plugins: [
+      new VueLoaderPlugin(),
+      new webpack.DefinePlugin({
+        'process.env': JSON.stringify(process.env),
+        'process.env.NODE_ENV': JSON.stringify(mode)
+      }),
     ]
-  },
-  plugins: [
-    new VueLoaderPlugin(),
-    new webpack.DefinePlugin({
-      'process.env': JSON.stringify(process.env),
-      'process.env.NODE_ENV': JSON.stringify(nodeEnv)
-    }),
-  ]
+  };
 };
-
-module.exports = config;
